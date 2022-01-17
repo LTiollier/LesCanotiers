fd0f5ec9d5cd8666b5ddad99550d5a3e3bd96b53
Léo TIOLLIER <ltiollier30@gmail.com>
2022-01-17 19:18:36 +0100

    :sparkles: Use virtual scroll for item select

--- resources/js/components/ListSelector.vue
+++ resources/js/components/ListSelector.vue
@@ -3,18 +3,23 @@
         class="mx-auto">
         <v-list>
             <v-list-item-group>
-                <v-list-item
-                    v-for="(item, i) in items"
-                    :key="i"
-                    @click="$emit('click', item)">
-                    <v-list-item-content>
-                        <v-list-item-title>
-                            <slot :item="item">
-                                {{ item.name }}
-                            </slot>
-                        </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
+                <v-virtual-scroll
+                    :items="items"
+                    height="300"
+                    item-height="64">
+                    <template v-slot:default="{ item }">
+                        <v-list-item
+                            @click="$emit('click', item)">
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    <slot :item="item">
+                                        {{ item.name }}
+                                    </slot>
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </template>
+                </v-virtual-scroll>
             </v-list-item-group>
         </v-list>
     </v-card>
